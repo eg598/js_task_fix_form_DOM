72abefb6a6fe3315ecdb593196125598f500bb23
Yegor <egor598000@gmail.com>
2025-12-25 01:06:31 +0100

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('.field-text');
+
+function formatLabel(item) {
+  item.insertAdjacentHTML(
+    'beforebegin',
+    `<label class="field-label" for="${item.id}">${item.name}</label>`,
+  );
+
+  item.setAttribute('placeholder', item.name.toLocaleUpperCase());
+}
+
+for (const item of inputs) {
+  formatLabel(item);
+}
